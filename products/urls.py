551f0583a918f29fe8966dae0424b1ad69e2fb6a
MIA1kl <mia01kln@gmail.com>
2023-06-29 19:35:56 +0600

    Fixed ProductDetailView

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,9 @@ from products.views import ProductListView, ProductUserView, ProductDetailView, 
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
-    path('products/<str:username>/', ProductUserView.as_view(), name='product-user'),
-    path('products/<str:username>/create/', ProductCreateView.as_view(), name='product-create'),
+    path('products/myproduct/<str:username>/', ProductUserView.as_view(), name='product-user'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('products/<str:username>/<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete'),
+    path('products/myproduct/<str:username>/create/', ProductCreateView.as_view(), name='product-create'),
+    path('products/myproduct/<str:username>/<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete'),
+
 ]
